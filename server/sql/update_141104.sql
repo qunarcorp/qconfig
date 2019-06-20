a9974d596ea5c1485282ae6239443d7ec1dc7b6e
pingyang.yang <pingyang.yang@qunar.com>
2019-06-20 15:49:04 +0800

    SQL修改

--- server/sql/update_141104.sql
+++
@@ -1,1 +0,0 @@
-alter table `config_used_log` add column `consumer_profile` varchar(50) not null default '' comment 'consumer的具体profile' after `source_profile`, add unique key `uniq_source_group_id_data_id_ip_port` (`source_group_id`,`source_data_id`,`ip`,`port`),drop index `uniq_source_group_id_data_id_profile_ip_port`;
